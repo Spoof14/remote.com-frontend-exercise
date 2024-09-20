021ca964fb1101887a7853b15704fa6b74168f18
Spoof14 <lars.manden@gmail.com>
2024-09-20 17:41:35 +0200

    Add files via upload

--- src/pages/People/peopleQueries.ts
+++ src/pages/People/peopleQueries.ts
@@ -2,8 +2,13 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import { Person } from 'types';
 import queryClient from 'utils/client';
 
-const fetchPeople = async (): Promise<Person[]> => {
-  return fetch(`http://localhost:4002/people`, { method: 'GET' }).then((response) => {
+const fetchPeople = async (search?: string, filters?: string[]): Promise<Person[]> => {
+  const params = new URLSearchParams();
+  search && params.append('name_like', search);
+  filters?.length === 1 && params.append('employment', filters.join(','));
+  return fetch(`http://localhost:4002/people${params.size > 0 ? '?' : ''}${params}`, {
+    method: 'GET',
+  }).then((response) => {
     if (!response.ok) {
       throw new Error('Failed to fetch people data');
     }
@@ -11,18 +16,38 @@ const fetchPeople = async (): Promise<Person[]> => {
   });
 };
 
-export const peopleKeys = {
-  all: ['people'] as const,
+const fetchPerson = async (id: number): Promise<Person> => {
+  return fetch(`http://localhost:4002/people/${id}`, { method: 'GET' }).then((response) =>
+    response.json(),
+  );
 };
 
-export const peopleQuery = () => ({
-  queryKey: peopleKeys.all,
-  queryFn: fetchPeople,
+export const peopleKeys = {
+  all: ['people'] as const,
+  filteredPeople: (search?: string, filters?: string[]) => ['people', search, filters] as const,
+  person: (id: number) => ['person', id] as const,
+};
+
+export const peopleQuery = (search?: string, filters?: string[]) => ({
+  queryKey: peopleKeys.filteredPeople(search, filters),
+  queryFn: () => fetchPeople(search, filters),
   throwOnError: true,
 });
 
-export const usePeople = () => {
-  return useSuspenseQuery(peopleQuery());
+export const personQuery = (id: number) => ({
+  queryKey: peopleKeys.person(id),
+  queryFn: () => fetchPerson(id),
+});
+
+export const usePerson = (id: number) => {
+  return useSuspenseQuery(personQuery(id));
+};
+
+export const usePeople = (search?: string, filters?: string[]) => {
+  let query;
+  if (!search && filters?.length === 0) query = peopleQuery();
+  else query = peopleQuery(search, filters);
+  return useSuspenseQuery(query);
 };
 
 export const peopleLoader =
